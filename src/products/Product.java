31b629a6e7547019d390111683c1674553d36400
21megha <111078321+21megha@users.noreply.github.com>
2026-01-14 00:43:26 +0530

    Add abstract Product class with basic properties

---
+++ src/products/Product.java
@@ -0,0 +1,36 @@
+package products;
+
+public abstract class Product {
+    protected String id;
+    protected String name;
+    protected double price;
+    protected String description;
+    protected int stockQuantity;
+
+    public Product(String id, String name, double price, String description, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public abstract double calculateDiscount();
+
+    public double getFinalPrice() {
+        return price - calculateDiscount();
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getStockQuantity() { return stockQuantity; }
+
+    public void displayInfo() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: ₹" + price);
+        System.out.println("Discount: ₹" + calculateDiscount());
+        System.out.println("Final Price: ₹" + getFinalPrice());
+    }
+}
